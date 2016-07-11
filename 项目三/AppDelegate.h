455e235ca37906245144c8e3e10e51d817bd1784
mac <mac@fuyou.local>
2016-07-11 18:59:43 +0800

    Initial Commit

---
+++ 项目三/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  项目三
+//
+//  Created by mac on 16/7/11.
+//  Copyright © 2016年 Liye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 项目三/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  项目三
+//
+//  Created by mac on 16/7/11.
+//  Copyright © 2016年 Liye. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
